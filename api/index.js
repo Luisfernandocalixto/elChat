1d279095e1bd07b78aad21b9154d992ed116a14f
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-08-08 22:47:51 -0600

    vercel 9.3

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-app.use(express.static('components'));
+app.use(express.static(path.join(__dirname, '..components')));
 const io = new Server(server, {
     connectionStateRecovery: {}
 })
@@ -29,57 +29,56 @@ await db.execute(`
         user TEXT
         )
         `);
-        
-        io.on("connection", async (socket) => {
-            console.log('a user has connected');
-            
-            socket.on("disconnect", () => {
-                console.log("an user has disconnected");
-            })
-            
-            socket.on("chat message", async (msg) => {
-                let result
-                const username = socket.handshake.auth.username ?? 'anonymous'
-                console.log({ username });
-                try {
-                    result = await db.execute({
-                        sql: 'INSERT INTO messages (content, user) VALUES (:msg , :username)',
-                        args: { msg, username }
-                    })
-                } catch (e) {
-                    console.error(e);
-                    return
-                }
-                io.emit("chat message", msg, result.lastInsertRowid.toString(), username);
-            })
-            
-            // recuperar los mensajes sin conexion
-            if (!socket.recovered) {
-                try {
-                    const result = await db.execute({
-                        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
-                        args: [socket.handshake.auth.serverOffset ?? 0]
-                    });
-                    
-                    result.rows.forEach(row => {
-                        socket.emit('chat message', row.content, row.id.toString(), row.user)
-                    });
-                } catch (e) {
-                    console.error(e);
-                }
-            }
-            
-        })
-        app.use(logger('dev'))
-        
-        app.get('/', async (req, res) => {
-            res.sendFile(process.cwd() + '/components/index.html')
-            // res.sendFile(path.join(__dirname + '/components/index.html'))
-        })
-        
-        
-        server.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+
+io.on("connection", async (socket) => {
+    console.log('a user has connected');
+
+    socket.on("disconnect", () => {
+        console.log("an user has disconnected");
     })
-    
-    
+
+    socket.on("chat message", async (msg) => {
+        let result
+        const username = socket.handshake.auth.username ?? 'anonymous'
+        console.log({ username });
+        try {
+            result = await db.execute({
+                sql: 'INSERT INTO messages (content, user) VALUES (:msg , :username)',
+                args: { msg, username }
+            })
+        } catch (e) {
+            console.error(e);
+            return
+        }
+        io.emit("chat message", msg, result.lastInsertRowid.toString(), username);
+    })
+
+    // recuperar los mensajes sin conexion
+    if (!socket.recovered) {
+        try {
+            const result = await db.execute({
+                sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            });
+
+            result.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString(), row.user)
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+})
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    // res.sendFile(process.cwd() + '/components/index.html')
+    res.sendFile(path.join(__dirname, '../components/index.html'))
+})
+
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+
